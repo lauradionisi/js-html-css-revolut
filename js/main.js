35e55c302683133dbc1ac5beb9ffa264cc73c4a3
Laura <laura.dionisi91@gmail.com>
2021-03-08 19:02:19 +0100

    Dropdown V_02

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
 
   $(".with-dropdown").mouseenter(function() {
-
-   $(this).find(".dropdown-menu").toggleClass("active");
    
-  });
+   $(this).find(".dropdown-menu").slideDown(500);
+   $(this).find(".dropdown-menu").addClass("active");
+   
 
+  });
+ 
+  $(".with-dropdown").mouseleave(function() {
+
+   $(this).find(".dropdown-menu").slideUp(500);
+   $(this).find(".dropdown-menu").removeClass("active");
+   
+
+  });
+   
 });
